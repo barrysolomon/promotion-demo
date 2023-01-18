51c7b5532610165166c22ef13a49fe2793e5c046
Barry David Solomon <barrysolomon@hotmail.com>
2023-01-18 16:03:57 -0700

    Add server log and env var output

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
-const express = require('express')
+/***
+ *  Notes
+ * 
+ *      npm install express
+ * 
+ *  Version History
+ * 
+ *      Version Date        Author          Notes
+ *      0.1     01/18/2023  Barry Solomon   Cloned from Eric's repo
+ * 
+ */
+
+const express = require('express');
+const os = require('os');
+
+const HOSTNAME = os.hostname();
 const PORT = process.env.PORT || 8080
+const SERVER_VERSION = "0.1";
 
 express()
     .get('/', (req, res) => {
-        res.send('Hello World!');
+
+        res.setHeader('Content-Type', 'text/plain');
+
+        //res.send('Hello World!');
+        res.statusCode = 200;
+
+        const ordered = Object.keys(process.env).sort().reduce(
+            (obj, key) => {
+                obj[key] = process.env[key];
+                return obj;
+            },
+            {}
+        );
+
+        var myArgs = process.argv;
+        var newDate = new Date();
+
+        res.end('Environment Variables\n\n' + JSON.stringify(ordered, null, 4) + '\n\nArguments:\n\n' + myArgs + '\n\nWelcome to CPLN! The time is: ' + newDate.toUTCString());
+
     })
-    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+    .listen(PORT, () => {
+        console.log(`Server version ${SERVER_VERSION} running at http://${HOSTNAME}:${PORT}/`)
+    });
