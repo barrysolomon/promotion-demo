f4ede8370b506b120240ffc2b627d19bfa582764
Barry David Solomon <barrysolomon@hotmail.com>
2023-01-19 09:01:51 -0700

    debugging

--- debug.sh
+++ debug.sh
@@ -3,7 +3,7 @@
 echo "CURRENT STATE OF WHATEVER"
 
 echo "-----cpln-gvc.yaml-----"
-cat cpln-gvc.yaml
+cat ./cpln/cpln-gvc.yaml
 echo "=> CPLN_GVC = ${CPLN_GVC}"
 echo "-----cpln-workload.yaml----- "
-cat cpln-workload.yaml 
+cat ./cpln/cpln-workload.yaml 
